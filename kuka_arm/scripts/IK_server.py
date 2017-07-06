df58dbd4368e8db648da990fc956d0544247da84
Carlos Olcina <karlos.olcina@gmail.com>
2017-07-06 02:51:06 +0200

    refactor: add q123 to the IK_server.py

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -17,8 +17,7 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from geometry_msgs.msg import Pose
 from mpmath import *
 from sympy import *
-
-
+#individual rotations
 def rot_x(q):
     R_x = Matrix([[ 1,              0,        0],
                   [ 0,        cos(q), -sin(q)],
@@ -35,6 +34,9 @@ def rot_z(q):
     R_z = Matrix([[ cos(q), -sin(q),        0],
                   [ sin(q),  cos(q),        0],
                   [ 0,              0,        1]])
+    return R_z
+# rad to deg conversion
+rad2deg = 180/np.pi
 
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
@@ -85,13 +87,76 @@ def handle_calculate_IK(req):
 
             # Calculate joint angles using Geometric IK method
 
+            Rrpy = rot_x(roll)*rot_y(pitch)*rot_z(yaw)
 
-            Rrpy = rot_x(roll) * rot_y(pitch) * rot_z(yaw)
-            R_x3 = Rrpy[0:2,2]
-            P_ee = Matrix([[px],[py],[pz]])
-		    WC_00 = P_ee - 0.303*R_x3
-            q1 = sympy.atan2(Wy,Wx)
-            print(q1)
+            P_ee = Matrix([px,py,pz])
+            R_x3 = Rrpy[0:3,2]
+            print('P_ee = ',P_ee)
+            print('R_x3 = ',R_x3)
+            #calculte the Wrist position
+
+            WC_00 = P_ee - d7*R_x3
+            WC_00 = WC_00.subs(s)
+            print('')
+            print('Wrist position --need checkin with the robot sym')
+            print('WC_00 = ',WC_00.subs(s))
+
+            #calculate q1
+            q1 = atan2(WC_00[1],WC_00[0]).subs(s)
+
+            print('q1 = ', q1)
+
+            #calculate q2 an q3
+
+            #first need the position of the WC in the 0_2 frame
+            WC_02 = [sqrt(WC_00[0]**2+WC_00[1]**2)-s[a1],WC_00[2]-s[d1]]
+            print('WC_02 = ',WC_02)
+
+            mod_WC_02 = sqrt(sum(i**2 for i in WC_02))
+            print('mod_WC_02 = ', mod_WC_02)
+
+            #calculate angle q34 and l34
+            q34 = atan2(s[a3],s[d4])
+            print('q34 = ', q34)
+            l34 = sqrt(s[a3]**2 + s[d4]**2)
+            print('l34 = ', l34)
+
+            #as we know l34, WC_02 and a2 we can calculate the angle between arms 2 an 3 using the cosine theorem
+            a = s[a2]
+            b = mod_WC_02
+            c = l34
+            angle_23 = acos((a**2 - b**2 + c**2)/(2*a*c))
+            print(a,b,c)
+            print('angle_23 =' , angle_23, ' in deg = ', angle_23*rad2deg)
+
+            #we can use te same theorem to calculate the angle q24
+            q24 = acos((b**2+a**2-c**2)/(2*b*a))
+
+            print('q24 =', q24, ' in deg = ', q24*rad2deg)
+
+            #now we can have q2 with 2 possible solutions
+            q2_1 = np.pi/2 -q24 - angle_23
+            q2_2 = np.pi/2 +q24 - angle_23
+            print('q2_1 = ', q2_1, ' in deg = ', q2_1*rad2deg)
+            print('q2_2 = ', q2_2, ' in deg = ', q2_2*rad2deg)
+            # and the same solutions for q3
+            q3_1 = np.pi +q34 - angle_23
+            q3_2 = np.pi -q34 - angle_23
+            print('q3_1 = ', q3_1, ' in deg = ', q3_1*rad2deg)
+            print('q3_2 = ', q3_2, ' in deg = ', q3_2*rad2deg)
+
+            q123 = {tetha1: q1, tetha2: q2_1, tetha3:q3_1}
+            #now we calculate R0_3
+            T0_3 = matrix_dict['T0_3'].subs(q123)
+            R0_3 = T0_3[0:3,0:3]
+
+            #and R3_6
+
+            R3_6 = R0_3.T * Rrpy
+
+            print(R3_6)
+            R3_6_anal = rot_z(tetha4)*rot_z(tetha5)*rot_z(tetha6)
+            print(simplify(R3_6_anal[0:3,0:3]))
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
 	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
