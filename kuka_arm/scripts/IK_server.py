a2636dd6aaafc4838558391b5b5513e0562d9da0
Carlos Olcina <karlos.olcina@gmail.com>
2017-07-10 12:19:12 +0200

    fix: add DH table params

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -65,7 +65,14 @@ def handle_calculate_IK(req):
 
 
             # Modified DH params
-
+            s = {alpha0:    0 , a0:      0  , d1:  0.75 ,
+                 alpha1:-pi/2 , a1:   0.35  , d2:     0 , tetha2: tetha2-pi/2,
+                 alpha2:    0 , a2:   1.25  , d3:     0 ,
+                 alpha3:-pi/2 , a3: -0.054  , d4:   1.5 ,
+                 alpha4: pi/2 , a4:      0  , d5:     0 ,
+                 alpha5:-pi/2 , a5:      0  , d6:     0 ,
+                 alpha6:    0 , a6:      0  , d7: 0.303 , tetha7:       0,
+            }
 
 
             # Define Modified DH Transformation matrix
